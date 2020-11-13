5657d24b399ecbca4effaf4aa42b49731516bdbe
lfal7838 <lashan.faliq@syscolabs.com>
2020-11-13 22:09:51 +0530

    Fix issues

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/users.js');
 const authenticateJWT = require('../middleware/auth');
 
-router.post('/', authenticateJWT, userController.createUser);
+router.post('/',  userController.createUser);
 
-router.get('/lawyers', authenticateJWT, userController.getLawyers);
+router.get('/lawyers', userController.getLawyers);
 
 router.get(
   '/lawyers/availability/:id',
