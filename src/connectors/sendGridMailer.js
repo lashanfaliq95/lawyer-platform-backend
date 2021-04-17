b60e60567219f64873b24b60142b2fe4e495bbf9
lfal7838 <lashan.faliq@syscolabs.com>
2021-04-17 20:56:19 +0530

    Fix package.json

--- src/connectors/sendGridMailer.js
+++
@@ -1,9 +0,0 @@
-const sgMail = require('@sendgrid/mail')
-
-if (process.env.NODE_ENV !== 'prod') {
-  require('dotenv').config();
-}
-
-sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
-
-module.exports=sgMail;
